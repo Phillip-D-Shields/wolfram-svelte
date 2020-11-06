1e5c8a319c2acb8c7fbd090ba3327ced804aec53
phillip <phillip@localhost.localdomain>
2020-11-06 20:15:46 +1300

    back to svelte, updated fetch

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  import Input from "./components/Input.svelte";
+  import { replies } from "./stores";
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 3em;
+    font-weight: 100;
+  }
+
+  h3 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 2em;
+    font-weight: 400;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Hello Dearie! Welcome to wee morrigans knowledge box</h1>
+  <Input />
+  {#if $replies}
+    {#each $replies as reply}
+      <h3>{reply}</h3>
+    {/each}
+  {/if}
+</main>
